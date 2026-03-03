aa704906a36426c874798ea0d045bde7a9476225
Benjamin <BenjaminSvensk@gmail.com>
2026-03-03 11:45:46 +0100

    goop

---
+++ VisitorVisitor/Assets/Main/Scripts/Player/PlayerController.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpForce = 10f;
+    
+    [Header("Hands")]
+    [SerializeField] private int currentHand;
+
+}
